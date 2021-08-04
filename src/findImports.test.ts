3f1f1bc934cac370076f6ba6c9fb2385dc7c12bf
Igor Korolev <missterr@gmail.com>
2021-08-04 13:12:29 +0400

    upgrade

--- src/findImports.test.ts
+++ src/findImports.test.ts
@@ -3,18 +3,31 @@ import * as ci from './collectImports';
 import * as ts from 'typescript';
 import { expect, use as chaiUse } from 'chai';
 import sinonChai from 'sinon-chai';
-import { findImports } from './findImports';
+import { findImports, findImportsSync } from './findImports';
 chaiUse(sinonChai);
 
 afterEach(() => {
   sinon.restore();
 });
 
-describe('findImports()', () => {
+const EMPTY_SET = new Set();
+describe('findImportsSync()', () => {
   it('opens a file and passes it to collectImports()', () => {
-    const fake = sinon.fake.returns(new Set());
+    const fake = sinon.fake.returns(EMPTY_SET);
     sinon.replace(ci, 'collectImports', fake);
-    expect(findImports(__filename)).to.be.deep.equal(new Set());
+    expect(findImportsSync(__filename)).to.be.equal(EMPTY_SET);
+    expect(fake).to.have.been.calledOnce;
+    const srcFile = fake.firstCall.args[0] as ts.SourceFile;
+    expect(srcFile).to.have.property('kind', ts.SyntaxKind.SourceFile);
+    expect(srcFile.fileName).to.be.equal(__filename);
+  });
+});
+
+describe('findImports()', () => {
+  it('opens a file and passes it to collectImports()', async () => {
+    const fake = sinon.fake.returns(EMPTY_SET);
+    sinon.replace(ci, 'collectImports', fake);
+    expect(await findImports(__filename)).to.be.equal(EMPTY_SET);
     expect(fake).to.have.been.calledOnce;
     const srcFile = fake.firstCall.args[0] as ts.SourceFile;
     expect(srcFile).to.have.property('kind', ts.SyntaxKind.SourceFile);
